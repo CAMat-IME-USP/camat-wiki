37f5650ec7be49db025829fb0831542dd070c7c4
Potalej <oapotalej@gmail.com>
2023-01-29 13:26:38 -0300

    Página com a lista dos RDs. Usa modais.

---
+++ js/rds_atuais.js
@@ -0,0 +1,105 @@
+/**
+ * CAMat-Wiki!
+ * Centro Acadêmico da Matemática, Estatística e Computação da Universidade de São Paulo
+ * https://camat.ime.usp.br/
+ * 
+ * Este é o javascript da página de RDs atuais do site. Para armazenar a informação dos RDs
+ * é utilizado um JSON que está no diretório "/_data".
+ * 
+ * Há dois tipos de modo noturno: um escuro (#212121) e um mais escuro (#111). Essa diferenciação 
+ * é feita em uma lista.
+ */
+
+// diretório dos contatos
+const DIR_CONTATOS = "/boletime_rds/rds/2023/contatos.json"
+
+// Captura o Modal
+var modal = document.getElementById("modal_infoRD")
+
+// Captura as informações do JSON usando uma Promise
+function capturarInfos () {
+  return new Promise(resolve => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200)
+        resolve(this.responseText)
+    }
+    xhttp.open("GET", DIR_CONTATOS, true)
+    xhttp.send()
+  })
+}
+
+// formatação do número de contato
+function formatarTelefone (numero) {
+  let ddd = numero.slice(0,2)
+  let primeiraParte = numero.slice(2,7)
+  let segundaParte = numero.slice(7,11)
+
+  return `(${ddd}) ${primeiraParte}-${segundaParte}`
+}
+
+function abrirModal () { modal.style.display = "flex" }
+
+// substituindo as informações no modal
+function atualizarModal (nome) {
+  // foto
+  let foto_rd = document.getElementById("foto_rd")
+  foto_rd.src = '/images/rds/carometro/' + infos[nome]["foto"]
+
+  // nome
+  let nome_rd = document.getElementById("nome_rd")
+  nome_rd.textContent = nome
+
+  // curso
+  let curso_rd = document.getElementById("curso_rd")
+  curso_rd.textContent = infos[nome]["curso"]
+
+  // cargos
+  let cargos_rd = document.getElementById("cargos_rd")
+  cargos_rd.innerHTML = ""
+  infos[nome]["cargos"].map(cargo => {
+      let li = document.createElement("li")
+      li.textContent = cargo
+      cargos_rd.appendChild(li)
+  })
+
+  // contato
+  let contato_rd = document.getElementById("contato_rd")
+  contato_rd.href = "tel:" + infos[nome]["contato"]
+  contato_rd.textContent = formatarTelefone(infos[nome]["contato"])
+
+  // abre o modal            
+  abrirModal()
+}
+
+/* EXECUTANDO */
+
+// onde são armazenadas as informações
+var infos = {}
+capturarInfos().then(res => {
+  infos = JSON.parse(res)
+})
+
+// capturar as âncoras
+var tds = document.getElementsByTagName('td')
+var ancoras = []
+for (let td of tds) {
+  // se só tiver 1, provavelmente é esse que se busca
+  if (td.children.length != 1) continue
+
+  let p = td.children[0]
+  let a = p.children[0]
+  
+  a.onclick = function() { atualizarModal(this.textContent) }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => modal.style.display = "none";
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) modal.style.display = "none";
+}
